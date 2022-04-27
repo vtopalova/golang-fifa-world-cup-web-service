dd191496b6c8c1a108196f11e1139bdf0cb0b0ee
I549296 <valentina.topalova@sap.com>
2022-04-27 13:06:58 +0200

    Implemented module 2, post handlers.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -33,6 +33,17 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	if !data.IsAccessTokenValid(accessToken) {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 
 }
 
